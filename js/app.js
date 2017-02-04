271116f5a4edd09f3cc31570d779a90d73ee8924
Alexandra-Fernandez <alexandrafernandezquispe@gmail.com>
2017-02-04 15:36:03 -0500

    proyecto chasq

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var contenedor = document.getElementById("loading");
+setTimeout(function() {
+    contenedor.classList.add("cerrar");
+  document.body.style.overflowX= "visible";
+}, 3000);
+
+
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
